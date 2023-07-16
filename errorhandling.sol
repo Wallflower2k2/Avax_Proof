a44228521a26236cc4109144b207807d431bb3f4
Wallflower2k2 <gaurikaushal18@gmail.com>
2023-07-17 01:04:26 +0530

    Week -3(project-1)

---
+++ errorhandling.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandling {
+    function requireStatement(uint x, uint y) public pure returns (uint) {
+        require(x > y, "x must be greater than y");
+        return x - y;
+    }
+    
+    function assertStatement(uint a, uint b) public pure returns (uint) {
+        assert(a > b);
+        return a + b;
+    }
+    
+    function revertStatement() public pure {
+        
+        revert("This function always reverts");
+    }
+}
